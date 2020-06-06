ba4952d521185374601a4fc5e87beb254a7082be
saif2020sami <65688107+saif2020sami@users.noreply.github.com>
2020-06-06 15:20:36 +0300

    Add files via upload

---
+++ ShuntDCparameters.m
@@ -0,0 +1,14 @@
+clear; %% Simulation of D.C. Machines Transient Behaviors: Teaching and Research by O.I. Okoro
+clc
+Bm=0.000001074;
+Tl=2.493;
+Va=24;
+Vf=12;
+ra=0.013;
+LAA=0.01;
+rf=1.43;
+LFF=0.167;
+LAF=0.004;
+%Kt=0.127;
+J=0.21;
+%Laf=0.9483;
